cbfb9c545be5fd7ca39abf9030c2d5f37b899f9b
Christopher Graham <ushi84@gmail.com>
2019-04-18 13:27:10 -0500

    Mongo DB Test
    Just a test to try out the C# Mongo Db driver and understand document based databases.

---
+++ MongoTest/MongoTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace MongoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var _client = new MongoClient("mongodb://localhost:27017");
+            var database = _client.GetDatabase("demodatabase");
+            var collection = database.GetCollection<Employee>("employee");
+
+            var Chris = new Employee
+            {
+                Name = "Chris Graham",
+                Profession = "Developer",
+                Pets = new List<Pet> { new Pet { Name = "Pikachu", Type = "Pokemon" } }
+            };
+
+             //collection.InsertOne(Chris);
+            var employees = collection.Find(x => x.Name == Chris.Name).ToList();
+            //var list = collection.Find(new BsonDocument("Name", "Jack")).ToList();
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine(emp.Pets[0].Name);
+            }
+
+        }
+    }
+
+    public class Employee
+    {
+        public BsonObjectId _id { get; set; }
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public List<Pet> Pets { get; set; }
+    }
+    public class Pet
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }  
+    }
+}
